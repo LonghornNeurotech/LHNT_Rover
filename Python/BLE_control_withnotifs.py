c0d767093fc9a8294f1eb1271488260933538438
Michael Chung <michaelc514718@gmail.com>
2025-01-28 21:53:24 -0600

    add input validation

--- Python/BLE_control_withnotifs.py
+++ Python/BLE_control_withnotifs.py
@@ -10,20 +10,35 @@ ESP_INPUT_CHARACTERISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 ESP_REPORT_CHARACTERISTIC_UUID = "2d21fb45-ea18-4b00-9fc9-53e55e2363be"
 ESP_NOTIFY_CHARACTERISTIC_UUID = "a654858a-5cbc-4bde-9f57-5bb967bd25b0"
 
+def is_valid_input(s):
+    if s.isnumeric():
+        num = int(s)
+        if num >= 0 and num <= 6:
+            return True # move command
+    elif len(s) == 1: # admin command no argument
+        if s[0].isalpha():
+            return True
+    elif len(s) > 2:
+        if s[0].isalpha() and s[1] == ' ' and s[2:].isnumeric(): # admin command with argument
+            return True
+    return False
 
 async def get_input(client):
     while True:
         print("Enter a command (or 'q' to quit) ", end="", flush=True)
-        val = await asyncio.to_thread(input)
-        if val.lower() == 'q':
+        val = (await asyncio.to_thread(input)).lower()
+        if val == 'q':
             print("Quitting...")
             await client.stop_notify(ESP_NOTIFY_CHARACTERISTIC_UUID)
             break
         # Write the string (as bytes) to the characteristic
-        await client.write_gatt_char(ESP_INPUT_CHARACTERISTIC_UUID, val.encode())
-        print(f"Sent '{val}' to the ESP32.")
+        if is_valid_input(val):
+            await client.write_gatt_char(ESP_INPUT_CHARACTERISTIC_UUID, val.encode())
+            print(f"Sent '{val}' to the ESP32.")
+        else:
+            print("\033[31mInvalid input! (Must be a number 0-6, an alpha character, or an alpha character and a number separated by a space)\033[0m")
 
-        if val.lower() == 'r':
+        if val == 'r':
             # Read the string (from bytes) from the characteristic
             byte_message = await client.read_gatt_char(ESP_REPORT_CHARACTERISTIC_UUID)
             message = byte_message.decode('utf-8')
@@ -52,3 +67,4 @@ async def main():
 
 # Run the main function
 asyncio.run(main())
+
